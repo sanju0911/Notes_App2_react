c15dbe5524b85aab9dcdb72322cc230c862e81ae
sanju <sanjayjanardhan231@gmail.com>
2024-09-13 13:19:59 +0530

    modifiying

---
+++ notes_app_react_advance/src/App.jsx
@@ -0,0 +1,12 @@
+import ADDNOTE from "./components/ADDNOTE";
+
+const APP = () => {
+  return (
+    <div>
+      <ADDNOTE></ADDNOTE>
+      <h1>hello world</h1>
+    </div>
+  );
+};
+
+export default APP;
---
+++ notes_app_react_advance/src/components/ADDNOTE.jsx
@@ -0,0 +1,16 @@
+const ADDNOTE = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Enter your note">
+        NAME :{" "}
+      </input>
+      <input type="text" placeholder="Enter your description">
+        DESCRIPTION :{" "}
+      </input>
+      <button>ADD</button>
+    </div>
+  );
+};
+
+
+export default ADDNOTE;
